84f8babc60ae651c48ad12e0b061476b08062aeb
Valerijs Farbtuhs <valerijs.farbtuhs@elnino.tech>
2025-09-28 12:52:04 +0200

    Added tailwind to the project

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-    <p>Hi, Val's page here</p>
+  <div class="bg-white dark:bg-primary h-svh">
+    <header class="h-1/10">
+      This is header
+    </header>
+    <main class="h-8/10 bg-accent/20 border-1 border-accent rounded-sm p-3">
+      <p class="font-bold underline">Hi, Val's page here</p>
+    </main>
+    <footer class="">
+      This is footer
+    </footer>
   </div>
 </template>

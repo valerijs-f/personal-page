88829f795de2eaffcec26f6d42875c5e4d9248b1
Valerijs Farbtuhs <valerijs.farbtuhs@elnino.tech>
2025-12-06 13:21:59 +0100

    Added font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,5 +14,15 @@ export default defineNuxtConfig({
   ],
   colorMode: {
     classSuffix: ''
-  }   
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css?family=Lato:300,400,700"
+        }
+      ]
+    }
+  }
 })

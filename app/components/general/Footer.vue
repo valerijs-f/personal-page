dd17d7bbb86f93129aee27f90f10a6604a9b490d
Valerijs Farbtuhs <valerijs.farbtuhs@elnino.tech>
2025-12-06 11:38:58 +0100

    Added footer content

--- app/components/general/Footer.vue
+++ app/components/general/Footer.vue
@@ -1,9 +1,13 @@
 <template>
-    <footer>
-      This is footer
-    </footer>
+  <footer class="p-3 border-t border-accent/20">
+    This website was coded in 
+    <a class="font-bold link" href="https://code.visualstudio.com/" target="_blank">VS Code</a>,
+    using <a class="font-bold link" href="https://nuxt.com/" target="_blank">Nuxt</a> with 
+    <a class="font-bold link" href="https://vuejs.org/" target="_blank">Vue</a> and JavaScript,
+    styled with <a class="font-bold link" href="https://tailwindcss.com/" target="_blank">Tailwind CSS</a>,
+    and built with <a class="font-bold link" href="https://vite.dev/" target="_blank">Vite</a>.
+    Deployment is handled by <a class="font-bold link" href="https://www.netlify.com/" target="_blank">Netlify</a>.
+    Inspired by Leonids Maslovs’ 
+    <a class="font-bold link" href="https://leonidsmaslovs.com/" target="_blank">portfolio website</a>.
+  </footer>
 </template>
-
-<script setup>
-
-</script>
